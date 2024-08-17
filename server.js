d1c586a3dfacff5bcda626467f55bfb7d7b1e0b2
Nolosts17 <mmtorres18@espe.edu.ec>
2024-08-17 15:34:06 -0500

    Actualizar server.js para usar variable de entorno PORT y configurar vercel.json

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // server.js
 const express = require('express');
 const app = express();
-const port = 3010;
+const port = process.env.PORT || 3010;
+
 
 // Middleware para parsear JSON
 app.use(express.json());
